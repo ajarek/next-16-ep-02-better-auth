eb32c604c98da11c07d3676825ab14beb3f2fc3e
ajarek <ajarek@poczta.onet.pl>
2025-11-10 01:00:17 +0100

    added forgot-password

--- app/forgot-password/page.tsx
+++ app/forgot-password/page.tsx
@@ -24,94 +24,110 @@ import {
 import { Input } from "@/components/ui/input"
 import Link from "next/link"
 import { Home } from "lucide-react"
+import { forgetPassword } from "@/lib/auth-client"
 
 
 const formSchema = z.object({
   email: z.email("Must be a valid email address"),
-  
- 
 })
 
-const ForgotPasswordPage= ()=> {
+const ForgotPasswordPage = () => {
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
       email: "",
-     
-      
     },
   })
 
-  function onSubmit(data: z.infer<typeof formSchema>) {
-    toast("You submitted the following values:", {
-      description: (
-        <pre className="text-primary mt-2 w-[320px] overflow-x-auto rounded-md p-4">
-          <code>{JSON.stringify(data, null, 2)}</code>
-        </pre>
-      ),
-      position: "bottom-right",
-      classNames: {
-        content: "flex flex-col gap-2",
-      },
-      style: {
-        "--border-radius": "calc(var(--radius)  + 4px)",
-      } as React.CSSProperties,
-    })
+  async function onSubmit(data: z.infer<typeof formSchema>) {
+    try {
+      const response = await forgetPassword({
+        email: data.email,
+        redirectTo: "/reset-password",
+      })
+      
+      if (response.error ) {
+        toast.error("Failed to send reset email" + response.error.message)
+      } else {
+        toast("You have requested a password reset:", {
+          description: (
+            <pre className='text-primary mt-2 w-[320px] overflow-x-auto rounded-md p-4'>
+              <code>{JSON.stringify(data, null, 2)}</code>
+            </pre>
+          ),
+          position: "bottom-right",
+          classNames: {
+            content: "flex flex-col gap-2",
+          },
+          style: {
+            "--border-radius": "calc(var(--radius)  + 4px)",
+          } as React.CSSProperties,
+        })
+        form.reset()
+      }
+    } catch (error) {
+      toast.error("Failed to log in" + error)
+    }
   }
 
   return (
-    <div className="flex min-h-screen items-center justify-center p-4">
-    <Card className="relative w-full sm:max-w-md shadow-xl border-4 border-primary/20">
-       <Link className='absolute top-4 right-4 text-blue-700 ' href="/"><Home/></Link>
-      <CardHeader>
-        <CardTitle>Forgot Password</CardTitle>
-        <CardDescription>
-          Enter your email to receive a password reset link
-        </CardDescription>
-      </CardHeader>
-      <CardContent>
-        <form id="form-rhf-demo" onSubmit={form.handleSubmit(onSubmit)}>
-          <FieldGroup>
-            <Controller
-              name="email"
-              
-              control={form.control}
-              render={({ field, fieldState }) => (
-                <Field data-invalid={fieldState.invalid}>
-                  <FieldLabel htmlFor="email">
-                    Email Address
-                  </FieldLabel>
-                  <Input
-                    {...field}
-                    id="email"
-                    type="email"
-                    aria-invalid={fieldState.invalid}
-                    placeholder="your-email@example.com"
-                    autoComplete="off"
-                  />
-                  {fieldState.invalid && (
-                    <FieldError errors={[fieldState.error]} />
-                  )}
-                </Field>
-              )}
-            />
-            
-            
-          </FieldGroup>
-        </form>
-      </CardContent>
-      <CardFooter className="flex flex-col gap-4">  
-        <Field orientation="horizontal">
-          <Button type="button" variant="outline" onClick={() => form.reset()}>
-            Reset
-          </Button>
-          <Button type="submit" form="form-rhf-demo">
-            Submit
-          </Button>
-        </Field>
-        <p><Link className='text-blue-700 hover:underline' href="/login">Back to Login</Link></p>
-      </CardFooter>
-    </Card>
+    <div className='flex min-h-screen items-center justify-center p-4'>
+      <Card className='relative w-full sm:max-w-md shadow-xl border-4 border-primary/20'>
+        <Link className='absolute top-4 right-4 text-blue-700 ' href='/'>
+          <Home />
+        </Link>
+        <CardHeader>
+          <CardTitle>Forgot Password</CardTitle>
+          <CardDescription>
+            Enter your email to receive a password reset link
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form id='form-rhf-demo' onSubmit={form.handleSubmit(onSubmit)}>
+            <FieldGroup>
+              <Controller
+                name='email'
+                control={form.control}
+                render={({ field, fieldState }) => (
+                  <Field data-invalid={fieldState.invalid}>
+                    <FieldLabel htmlFor='email'>Email Address</FieldLabel>
+                    <Input
+                      {...field}
+                      id='email'
+                      type='email'
+                      aria-invalid={fieldState.invalid}
+                      placeholder='your-email@example.com'
+                      autoComplete='off'
+                    />
+                    {fieldState.invalid && (
+                      <FieldError errors={[fieldState.error]} />
+                    )}
+                  </Field>
+                )}
+              />
+            </FieldGroup>
+          </form>
+        </CardContent>
+        <CardFooter className='flex flex-col gap-4'>
+          <Field orientation='horizontal'>
+            <Button
+              type='button'
+              variant='outline'
+              onClick={() => form.reset()}
+            >
+              Reset
+            </Button>
+            <Button type='submit' form='form-rhf-demo'>
+              Submit
+            </Button>
+          </Field>
+          <p>
+            <Link className='text-blue-700 hover:underline' href='/login'>
+              Back to Login
+            </Link>
+          </p>
+        </CardFooter>
+      </Card>
     </div>
   )
 }
--- lib/auth.ts
+++ lib/auth.ts
@@ -1,14 +1,17 @@
-import { betterAuth } from "better-auth";
-import { prismaAdapter } from "better-auth/adapters/prisma";
+import { betterAuth } from "better-auth"
+import { prismaAdapter } from "better-auth/adapters/prisma"
 // If your Prisma file is located elsewhere, you can change the path
-import { PrismaClient } from "@/lib/generated/prisma/client";
+import { PrismaClient } from "@/lib/generated/prisma/client"
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient()
 export const auth = betterAuth({
-    database: prismaAdapter(prisma, {
-        provider: "postgresql", // or "mysql", "postgresql", ...etc
-    }),
-    emailAndPassword:{
-        enabled: true
-    }
-});
+  database: prismaAdapter(prisma, {
+    provider: "postgresql", // or "mysql", "postgresql", ...etc
+  }),
+  emailAndPassword: {
+    enabled: true,
+    sendResetPassword: async ({ user, url }) => {
+      console.log("Reset password url for", user.email, ":", url)
+    },
+  },
+})
